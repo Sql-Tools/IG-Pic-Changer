f3a44e8319112df792f23288809daa214eefddc1
Sql-Tools <73824369+Sql-Tools@users.noreply.github.com>
2020-11-09 14:01:43 +0200

    Add Main [SRC] In Repository

---
+++ IG-Change-Profile-Pic-[SRC]/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.UserBox = New System.Windows.Forms.TextBox()
+        Me.PassBox = New System.Windows.Forms.TextBox()
+        Me.AllInOne = New System.Windows.Forms.Button()
+        Me.NewPic = New System.Windows.Forms.PictureBox()
+        Me.Hi = New System.Windows.Forms.Panel()
+        Me.P4 = New System.Windows.Forms.Splitter()
+        Me.P3 = New System.Windows.Forms.Splitter()
+        Me.P2 = New System.Windows.Forms.Splitter()
+        Me.P1 = New System.Windows.Forms.Splitter()
+        Me.PicListBox = New System.Windows.Forms.ListBox()
+        Me.Counter = New System.Windows.Forms.Label()
+        CType(Me.NewPic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Hi.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'UserBox
+        '
+        Me.UserBox.ForeColor = System.Drawing.Color.Gray
+        Me.UserBox.Location = New System.Drawing.Point(12, 11)
+        Me.UserBox.Name = "UserBox"
+        Me.UserBox.Size = New System.Drawing.Size(50, 20)
+        Me.UserBox.TabIndex = 0
+        Me.UserBox.Text = "User"
+        Me.UserBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'PassBox
+        '
+        Me.PassBox.ForeColor = System.Drawing.Color.Gray
+        Me.PassBox.Location = New System.Drawing.Point(61, 11)
+        Me.PassBox.Name = "PassBox"
+        Me.PassBox.Size = New System.Drawing.Size(50, 20)
+        Me.PassBox.TabIndex = 1
+        Me.PassBox.Text = "Pass"
+        Me.PassBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.PassBox.UseSystemPasswordChar = True
+        '
+        'AllInOne
+        '
+        Me.AllInOne.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.AllInOne.ForeColor = System.Drawing.Color.Gray
+        Me.AllInOne.Location = New System.Drawing.Point(12, 30)
+        Me.AllInOne.Name = "AllInOne"
+        Me.AllInOne.Size = New System.Drawing.Size(99, 23)
+        Me.AllInOne.TabIndex = 2
+        Me.AllInOne.Text = "Login"
+        Me.AllInOne.UseVisualStyleBackColor = True
+        '
+        'NewPic
+        '
+        Me.NewPic.Location = New System.Drawing.Point(0, 0)
+        Me.NewPic.Name = "NewPic"
+        Me.NewPic.Size = New System.Drawing.Size(145, 97)
+        Me.NewPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.NewPic.TabIndex = 4
+        Me.NewPic.TabStop = False
+        '
+        'Hi
+        '
+        Me.Hi.Controls.Add(Me.Counter)
+        Me.Hi.Controls.Add(Me.P4)
+        Me.Hi.Controls.Add(Me.P3)
+        Me.Hi.Controls.Add(Me.P2)
+        Me.Hi.Controls.Add(Me.P1)
+        Me.Hi.Controls.Add(Me.NewPic)
+        Me.Hi.Location = New System.Drawing.Point(119, 11)
+        Me.Hi.Name = "Hi"
+        Me.Hi.Size = New System.Drawing.Size(145, 97)
+        Me.Hi.TabIndex = 5
+        '
+        'P4
+        '
+        Me.P4.BackColor = System.Drawing.Color.Black
+        Me.P4.Cursor = System.Windows.Forms.Cursors.Default
+        Me.P4.Dock = System.Windows.Forms.DockStyle.Right
+        Me.P4.Location = New System.Drawing.Point(144, 1)
+        Me.P4.Name = "P4"
+        Me.P4.Size = New System.Drawing.Size(1, 95)
+        Me.P4.TabIndex = 3
+        Me.P4.TabStop = False
+        '
+        'P3
+        '
+        Me.P3.BackColor = System.Drawing.Color.Black
+        Me.P3.Cursor = System.Windows.Forms.Cursors.Default
+        Me.P3.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.P3.Location = New System.Drawing.Point(1, 96)
+        Me.P3.Name = "P3"
+        Me.P3.Size = New System.Drawing.Size(144, 1)
+        Me.P3.TabIndex = 2
+        Me.P3.TabStop = False
+        '
+        'P2
+        '
+        Me.P2.BackColor = System.Drawing.Color.Black
+        Me.P2.Cursor = System.Windows.Forms.Cursors.Default
+        Me.P2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.P2.Location = New System.Drawing.Point(1, 0)
+        Me.P2.Name = "P2"
+        Me.P2.Size = New System.Drawing.Size(144, 1)
+        Me.P2.TabIndex = 1
+        Me.P2.TabStop = False
+        '
+        'P1
+        '
+        Me.P1.BackColor = System.Drawing.Color.Black
+        Me.P1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.P1.Location = New System.Drawing.Point(0, 0)
+        Me.P1.Name = "P1"
+        Me.P1.Size = New System.Drawing.Size(1, 97)
+        Me.P1.TabIndex = 0
+        Me.P1.TabStop = False
+        '
+        'PicListBox
+        '
+        Me.PicListBox.FormattingEnabled = True
+        Me.PicListBox.Location = New System.Drawing.Point(12, 52)
+        Me.PicListBox.Name = "PicListBox"
+        Me.PicListBox.Size = New System.Drawing.Size(99, 56)
+        Me.PicListBox.TabIndex = 6
+        '
+        'Counter
+        '
+        Me.Counter.AutoSize = True
+        Me.Counter.BackColor = System.Drawing.Color.Transparent
+        Me.Counter.Font = New System.Drawing.Font("Tahoma", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.Counter.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Counter.Location = New System.Drawing.Point(58, 81)
+        Me.Counter.Name = "Counter"
+        Me.Counter.Size = New System.Drawing.Size(28, 13)
+        Me.Counter.TabIndex = 5
+        Me.Counter.Text = "0|0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(277, 121)
+        Me.Controls.Add(Me.PicListBox)
+        Me.Controls.Add(Me.Hi)
+        Me.Controls.Add(Me.AllInOne)
+        Me.Controls.Add(Me.PassBox)
+        Me.Controls.Add(Me.UserBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "IG-Pic-Changer"
+        CType(Me.NewPic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Hi.ResumeLayout(False)
+        Me.Hi.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents UserBox As TextBox
+    Friend WithEvents PassBox As TextBox
+    Friend WithEvents AllInOne As Button
+    Friend WithEvents NewPic As PictureBox
+    Friend WithEvents Hi As Panel
+    Friend WithEvents P4 As Splitter
+    Friend WithEvents P3 As Splitter
+    Friend WithEvents P2 As Splitter
+    Friend WithEvents P1 As Splitter
+    Friend WithEvents PicListBox As ListBox
+    Friend WithEvents Counter As Label
+End Class
---
+++ IG-Change-Profile-Pic-[SRC]/Form1.vb
@@ -0,0 +1,136 @@
+﻿Imports System.Net, System.Text.RegularExpressions, System.IO
+
+Public Class Form1
+#Region "Publics"
+    Public FullCookie As String : Public Bool As Boolean
+    Public G As String = "0" : Public B As String = "0"
+#End Region
+#Region "Controls"
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Microsoft.VisualBasic.CompilerServices.ProjectData.EndApp() 'Due To We Have Loop And Thread Deamon Started We Must Exit All Opreations
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False 'Analyze Cross Thread Calls In Form :) Caused By Controls
+        ServicePointManager.UseNagleAlgorithm = False 'Analyze Internet :)
+        ServicePointManager.Expect100Continue = False 'Analyze Internet :)
+        Dim DirFolder As New DirectoryInfo("Pics/") 'Get Directory Of The Folder To Get *Pics Names
+        Dim Dir2 As FileInfo() = DirFolder.GetFiles()
+        For Each FileName In Dir2
+            PicListBox.Items.Add("Pics/" + FileName.ToString())
+        Next
+    End Sub
+    Private Sub AllInOne_Click(sender As Object, e As EventArgs) Handles AllInOne.Click
+        If AllInOne.Text = "Login" Then
+            Dim T As Threading.Thread = New Threading.Thread(Sub()
+                                                                 Dim LoginResp As String = Login(UserBox.Text, PassBox.Text)
+                                                                 If LoginResp.Contains("""authenticated"": true, """) Then MsgBox("Done Login") : AllInOne.Text = "Start" Else MsgBox($"Error Login Reason Is : {vbCrLf}{LoginResp}")
+                                                             End Sub) With {
+                                                         .IsBackground = False
+                                                         } : T.Start()
+        ElseIf AllInOne.Text = "Start" Then
+            PicListBox.SelectedIndex = 0 : Me.Bool = False
+            Dim T As Threading.Thread = New Threading.Thread(New Threading.ThreadStart(AddressOf Work)) With {
+                                                             .IsBackground = False} : T.Start() : AllInOne.Text = "Stop"
+        ElseIf AllInOne.Text = "Stop" Then
+            AllInOne.Text = "Start" : Me.Bool = True
+        End If
+    End Sub
+    Sub Work()
+        While Not Me.Bool
+            Dim ImgFile As String = PicListBox.SelectedItem : NewPic.Image = Image.FromFile(ImgFile)
+            Dim ImgBytes() As Byte = File.ReadAllBytes(ImgFile)
+            If Change(System.Text.Encoding.Default.GetString(ImgBytes)) Then
+                G += 1 : Counter.Text = $"{G}|{B}" : Try : PicListBox.SelectedIndex += 1 : Catch ex As Exception : PicListBox.SelectedIndex = 0 : End Try
+            Else : B += 1 : Counter.Text = $"{G}|{B}" : Try : PicListBox.SelectedIndex += 1 : Catch ex As Exception : PicListBox.SelectedIndex = 0 : End Try : End If
+            Threading.Thread.Sleep(New Random().Next(2, 9))
+        End While
+    End Sub
+#End Region
+#Region "AJAX Connections"
+    Private Function Login(User As String, Pass As String) As String
+        Dim Resp As String = ""
+        Try : Net.ServicePointManager.CheckCertificateRevocationList = False : Net.ServicePointManager.DefaultConnectionLimit = 300 : Net.ServicePointManager.UseNagleAlgorithm = False : Net.ServicePointManager.Expect100Continue = False : Net.ServicePointManager.SecurityProtocol = 3072
+            Dim Encoding As New Text.UTF8Encoding
+            Dim Bytes As Byte() = Encoding.GetBytes("username=" + User + "&enc_password=#PWD_INSTAGRAM_BROWSER:0:0000000000:" + Pass + "&queryParams={}&optIntoOneTap=false")
+            Dim AJ As Net.HttpWebRequest = DirectCast(Net.WebRequest.Create("https://www.instagram.com/accounts/login/ajax/"), Net.HttpWebRequest)
+            With AJ
+                .Method = "POST"
+                .Accept = "*/*"
+                .AutomaticDecompression = Net.DecompressionMethods.Deflate Or Net.DecompressionMethods.GZip
+                .Headers.Add("Accept-Language: en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,bas-CM;q=0.6,bas;q=0.5")
+                .KeepAlive = True
+                .ContentLength = Bytes.Length
+                .ContentType = "application/x-www-form-urlencoded"
+                .Headers.Add("Cookie: mid=missing; csrftoken=missing;")
+                .Host = "www.instagram.com"
+                .UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+                .Headers.Add("X-CSRFToken: missing")
+                .Headers.Add("X-IG-App-ID: 936619743392459")
+                .Headers.Add("X-Instagram-AJAX: b9c0ea6d7d91-hot")
+                .Headers.Add("X-Requested-With: XMLHttpRequest")
+            End With
+            Dim Stream As IO.Stream = AJ.GetRequestStream() : Stream.Write(Bytes, 0, Bytes.Length) : Stream.Dispose() : Stream.Close()
+            Dim Reader As New IO.StreamReader(DirectCast(AJ.GetResponse(), Net.HttpWebResponse).GetResponseStream()) : Resp = Reader.ReadToEnd : Reader.Dispose() : Reader.Close()
+            Dim CookieGrab As HttpWebResponse = DirectCast(AJ.GetResponse(), HttpWebResponse)
+            Dim RegexSetting As String = CookieGrab.GetResponseHeader("Set-Cookie")
+            If Resp.Contains("""authenticated"": true, """) Then
+#Region "Cookies"
+                Dim Csrftoken As String = New Regex("csrftoken=(.*?);").Match(RegexSetting).Groups(1).Value
+                Dim Mid As String = New Regex("mid=(.*?);").Match(RegexSetting).Groups(1).Value
+                Dim Rur As String = New Regex("rur=(.*?);").Match(RegexSetting).Groups(1).Value
+                Dim SessionId As String = New Regex("sessionid=(.*?);").Match(RegexSetting).Groups(1).Value
+                Dim DsUserId As String = New Regex("ds_user_id=(.*?);").Match(RegexSetting).Groups(1).Value
+                Dim DsUser As String = New Regex("ds_user=(.*?);").Match(RegexSetting).Groups(1).Value
+                Dim igdid As String = New Regex("ig_did=(.*?);").Match(RegexSetting).Groups(1).Value
+#End Region
+                FullCookie = "is_starred_enabled=yes; sessionid=" + SessionId + "; mid=" + Mid + "; ds_user=" + DsUser + "; ds_user_id=" + DsUserId + "; csrftoken=" + Csrftoken + "; igfl=" + DsUser + "; ig_direct_region_hint=" + Rur + "; rur=" + Rur + ";"
+            End If
+        Catch ex As WebException
+            Resp = New IO.StreamReader(ex.Response.GetResponseStream()).ReadToEnd()
+        End Try
+        Return Resp
+    End Function
+    Private Function Change(PictureBytesConvert As String) As Boolean
+        Dim IsPosted As Boolean = False
+        Try
+#Region "Bytes Solver"
+            Dim Boundary As String = DateTime.Now.Ticks.ToString("x") 'Get Form Boundary 
+            Dim PostDataSB As New Text.StringBuilder() 'Build The Right Data In Lines Format So Must Use .AppendLine Not .Append
+            PostDataSB.AppendLine($"------WebKitFormBoundary{Boundary}") '1st Line
+            PostDataSB.AppendLine("Content-Disposition: form-data; name=""profile_pic""; filename=""profilepic.jpg""") '2nd Line
+            PostDataSB.AppendLine("Content-Type: image/jpeg") '3rd Line
+            PostDataSB.AppendLine(String.Empty) 'Make Space
+            PostDataSB.AppendLine(PictureBytesConvert) 'Get PictureBytes In .Default Encoding **If You Use UTF8 Status Code Will Be 408
+            PostDataSB.AppendLine($"------WebKitFormBoundary{Boundary}--") 'Append Last Line :)
+            Dim Bytes As Byte() = System.Text.Encoding.Default.GetBytes(PostDataSB.ToString()) 'Get All Data Have Appended In StringBuilder
+#End Region
+            Dim AJ As Net.HttpWebRequest = Net.WebRequest.CreateHttp("https://www.instagram.com/accounts/web_change_profile_picture/")
+#Region "Shitty Headers :)"
+            With AJ
+                .Method = "POST"
+                .Host = "www.instagram.com"
+                .KeepAlive = True
+                .ContentLength = Bytes.Length
+                .Headers.Add("X-IG-WWW-Claim: hmac.AR0KX23gPNOJFSnta7iaQcTPPDM622f4k3wBe8LiwnC2AnyT")
+                .Headers.Add("X-Instagram-AJAX: b9c0ea6d7d91-hot")
+                .ContentType = $"multipart/form-data; boundary=----WebKitFormBoundary{Boundary}"
+                .Accept = "*/*"
+                .UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36"
+                .Headers.Add("X-Requested-With: XMLHttpRequest")
+                .Headers.Add(HttpRequestHeader.Cookie, FullCookie)
+                .Headers.Add("X-CSRFToken", Regex.Match(FullCookie, "csrftoken=(.*?);").Groups(1).Value)
+                .Headers.Add("X-IG-App-ID: 936619743392459")
+                .AutomaticDecompression = Net.DecompressionMethods.Deflate Or Net.DecompressionMethods.GZip
+                .Headers.Add(HttpRequestHeader.AcceptLanguage, "en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,bas-CM;q=0.6,bas;q=0.5")
+            End With
+#End Region
+            Dim Stream As IO.Stream = AJ.GetRequestStream() : Stream.Write(Bytes, 0, Bytes.Length) : Stream.Dispose() : Stream.Close()
+            Dim Reader As New IO.StreamReader(DirectCast(AJ.GetResponse(), Net.HttpWebResponse).GetResponseStream()) : Dim Text As String = Reader.ReadToEnd : Reader.Dispose() : Reader.Close()
+            If Text.Contains("""changed_profile"": true, """) Then IsPosted = True
+        Catch ex As WebException : Dim AJJ As String = New IO.StreamReader(ex.Response.GetResponseStream()).ReadToEnd()
+            IsPosted = False
+        End Try
+        Return IsPosted
+    End Function
+#End Region
+End Class
